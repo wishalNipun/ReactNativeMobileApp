09039f5dda57e7ee013fb53e065de7e0ee33ed05
wishalNipun <wishal1974@gmail.com>
2024-02-23 23:04:43 +0530

    Home created

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
-import React, {useState} from 'react';
-import {Alert, Button, SafeAreaView, Text, TextInput, View} from 'react-native';
-
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import Home from './src/components/screens/Home';
+import MyCart from './src/components/screens/MyCart';
+import ProductInfo from './src/components/screens/ProductInfo';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 function App(): React.JSX.Element {
  
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={{flex: 1, backgroundColor: 'blue'}}>
-      
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="MyCart" component={MyCart} />
+      <Stack.Screen name="ProductInfo" component={ProductInfo} />
+    </Stack.Navigator>
+  </NavigationContainer>
   );
 }
 
---
+++ src/components/screens/Home.tsx
@@ -0,0 +1,346 @@
+import {
+  View,
+  Text,
+  StatusBar,
+  ScrollView,
+  TouchableOpacity,
+  Image,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {COLOURS, Item, Items} from '../database/Database';
+import Entypo from 'react-native-vector-icons/Entypo';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
+const Home = ({navigation}: any) => {
+  const [products, setProducts] = useState<Item[]>([]);
+  const [accessory, setAccessory] = useState<Item[]>([]);
+
+  //get called on screen loads
+  useEffect(() => {
+    const nav = navigation.addListener('focus', () => {
+      getDataFromDB();
+    });
+
+    return nav;
+  }, [navigation]);
+
+  //get data from DB
+
+  const getDataFromDB = () => {
+    let productList = [];
+    let accessoryList = [];
+    for (let index = 0; index < Items.length; index++) {
+      if (Items[index].category == 'product') {
+        productList.push(Items[index]);
+      } else if (Items[index].category == 'accessory') {
+        accessoryList.push(Items[index]);
+      }
+    }
+
+    setProducts(productList);
+    setAccessory(accessoryList);
+  };
+
+  //create an product reusable card
+
+  const ProductCard = ({data}: any) => {
+    return (
+      <TouchableOpacity
+        onPress={() => navigation.navigate('ProductInfo', {productID: data.id})}
+        style={{
+          width: '48%',
+          marginVertical: 14,
+        }}>
+        <View
+          style={{
+            width: '100%',
+            height: 100,
+            borderRadius: 10,
+            backgroundColor: COLOURS.backgroundLight,
+            position: 'relative',
+            justifyContent: 'center',
+            alignItems: 'center',
+            marginBottom: 8,
+          }}>
+          {data.isOff ? (
+            <View
+              style={{
+                position: 'absolute',
+                width: '20%',
+                height: '24%',
+                backgroundColor: COLOURS.green,
+                top: 0,
+                left: 0,
+                borderTopLeftRadius: 10,
+                borderBottomRightRadius: 10,
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
+              <Text
+                style={{
+                  fontSize: 12,
+                  color: COLOURS.white,
+                  fontWeight: 'bold',
+                  letterSpacing: 1,
+                }}>
+                {data.offPercentage}%
+              </Text>
+            </View>
+          ) : null}
+          <Image
+            source={data.productImage}
+            style={{
+              width: '80%',
+              height: '80%',
+              resizeMode: 'contain',
+            }}
+          />
+        </View>
+        <Text
+          style={{
+            fontSize: 12,
+            color: COLOURS.black,
+            fontWeight: '600',
+            marginBottom: 2,
+          }}>
+          {data.productName}
+        </Text>
+        {data.category == 'accessory' ? (
+          data.isAvailable ? (
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}>
+              <FontAwesome
+                name="circle"
+                style={{
+                  fontSize: 12,
+                  marginRight: 6,
+                  color: COLOURS.green,
+                }}
+              />
+              <Text
+                style={{
+                  fontSize: 12,
+                  color: COLOURS.green,
+                }}>
+                Available
+              </Text>
+            </View>
+          ) : (
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}>
+              <FontAwesome
+                name="circle"
+                style={{
+                  fontSize: 12,
+                  marginRight: 6,
+                  color: COLOURS.red,
+                }}
+              />
+              <Text
+                style={{
+                  fontSize: 12,
+                  color: COLOURS.red,
+                }}>
+                Unavailable
+              </Text>
+            </View>
+          )
+        ) : null}
+        <Text>LKR {data.productPrice}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+  return (
+    <View
+      style={{
+        width: '100%',
+        height: '100%',
+        backgroundColor: COLOURS.white,
+      }}>
+      <StatusBar backgroundColor={COLOURS.white} barStyle="dark-content" />
+      <ScrollView showsVerticalScrollIndicator={true}>
+        <View
+          style={{
+            width: '100%',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            padding: 16,
+          }}>
+          <TouchableOpacity>
+            <Entypo
+              name="shopping-bag"
+              style={{
+                fontSize: 18,
+                color: COLOURS.backgroundMedium,
+                padding: 12,
+                borderRadius: 10,
+                backgroundColor: COLOURS.backgroundLight,
+              }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => navigation.navigate('MyCart')}>
+            <MaterialCommunityIcons
+              name="cart"
+              style={{
+                fontSize: 18,
+                color: COLOURS.backgroundMedium,
+                padding: 12,
+                borderRadius: 10,
+                borderWidth: 1,
+                borderColor: COLOURS.backgroundLight,
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            marginBottom: 10,
+            padding: 16,
+          }}>
+          <Text
+            style={{
+              fontSize: 26,
+              color: COLOURS.black,
+              fontWeight: '500',
+              letterSpacing: 1,
+              marginBottom: 10,
+            }}>
+            WELCOME WISHAL SOPPING STORE !
+          </Text>
+          <Text
+            style={{
+              fontSize: 14,
+              color: COLOURS.black,
+              fontWeight: '400',
+              letterSpacing: 1,
+              lineHeight: 24,
+            }}>
+            shop on Moragalla Beruwala Sri Lanaka.
+            {'\n'}App only benifits make shopping faster and easier
+          </Text>
+        </View>
+        <View
+          style={{
+            padding: 16,
+          }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}>
+              <Text
+                style={{
+                  fontSize: 18,
+                  color: COLOURS.black,
+                  fontWeight: '500',
+                  letterSpacing: 1,
+                }}>
+                Products
+              </Text>
+              <Text
+                style={{
+                  fontSize: 14,
+                  color: COLOURS.black,
+                  fontWeight: '400',
+                  opacity: 0.5,
+                  marginLeft: 10,
+                }}>
+                50
+              </Text>
+            </View>
+            <Text
+              style={{
+                fontSize: 14,
+                color: COLOURS.blue,
+                fontWeight: '400',
+              }}>
+              SeeAll
+            </Text>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              flexWrap: 'wrap',
+              justifyContent: 'space-around',
+            }}>
+            {products.map(data => {
+              return <ProductCard data={data} key={data.id} />;
+            })}
+          </View>
+        </View>
+
+        <View
+          style={{
+            padding: 16,
+          }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}>
+            <View
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}>
+              <Text
+                style={{
+                  fontSize: 18,
+                  color: COLOURS.black,
+                  fontWeight: '500',
+                  letterSpacing: 1,
+                }}>
+                Accessories
+              </Text>
+              <Text
+                style={{
+                  fontSize: 14,
+                  color: COLOURS.black,
+                  fontWeight: '400',
+                  opacity: 0.5,
+                  marginLeft: 10,
+                }}>
+                80
+              </Text>
+            </View>
+            <Text
+              style={{
+                fontSize: 14,
+                color: COLOURS.blue,
+                fontWeight: '400',
+              }}>
+              SeeAll
+            </Text>
+          </View>
+          <View
+            style={{
+              flexDirection: 'row',
+              flexWrap: 'wrap',
+              justifyContent: 'space-around',
+            }}>
+            {accessory.map(data => {
+              return <ProductCard data={data} key={data.id} />;
+            })}
+          </View>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
+
+export default Home;
