a3cd91217b6ae8bee1466f00fb74b8f5248e9950
wishalNipun <wishal1974@gmail.com>
2024-02-23 14:25:51 +0530

    1st commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import {Alert, Button, SafeAreaView, Text, TextInput, View} from 'react-native';
+
+function App(): React.JSX.Element {
+ 
+  return (
+    <View style={{flex: 1, backgroundColor: 'blue'}}>
+      
+    </View>
+  );
+}
+
+export default App;
