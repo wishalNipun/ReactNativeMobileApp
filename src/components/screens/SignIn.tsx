4e41ce61fe3b354e85d621a960e23e7073ef132f
wishalNipun <wishal1974@gmail.com>
2024-02-28 00:40:07 +0530

    created user registration and login also added validation

--- src/components/screens/SignIn.tsx
+++ src/components/screens/SignIn.tsx
@@ -1,133 +1,199 @@
-import {View, Text, TouchableOpacity, Button, TextInput} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  Keyboard,
+  ToastAndroid,
+} from 'react-native';
+import React, {useState} from 'react';
 import {COLOURS} from '../database/Database';
 import {Image} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import DismissKeyboard from '../components/DismissKeyboard';
 
 const SignIn = ({navigation}: any) => {
-  return (
-    <View style={{flex: 1, backgroundColor: 'white'}}>
-      <Image
-        style={{
-          width: 200,
-          height: 200,
-          alignSelf: 'center',
-          marginTop: 50,
-          borderRadius: 50,
-        }}
-        source={require('../database/images/login/logo.png')}
-      />
-      <View style={{alignItems: 'center', marginTop: 70}}>
-        <Text
-          style={{
-            color: COLOURS.blue,
-            fontSize: 40,
-            fontWeight: '800',
-            fontFamily: 'roboto',
-            letterSpacing: 1.5,
-            marginBottom: 20,
-          }}>
-          SIGN IN
-        </Text>
-        <View
-          style={{
-            height: 55,
-            backgroundColor: COLOURS.light,
-            flexDirection: 'row',
-            paddingHorizontal: 15,
-            borderWidth: 0.5,
-            width: '86%',
-            borderRadius: 5,
-            marginBottom: 20,
-          }}>
-          <TextInput
-            style={{
-              height: 55,
-              width: '86%',
-              color: COLOURS.black,
-              fontSize: 14,
-            }}
-            placeholderTextColor="#000"
-            placeholder="Email"
-          />
-        </View>
+  const [inputs, setInputs] = useState({email: '', password: ''});
 
-        <View
+  const validate = async () => {
+    Keyboard.dismiss();
+    let isValid = true;
+    if (!inputs.email) {
+      isValid = false;
+    }
+    if (!inputs.password) {
+      isValid = false;
+    }
+    if (isValid) {
+      login();
+    } else {
+      ToastAndroid.show('Enter Email And Password !', ToastAndroid.SHORT);
+    }
+  };
+
+  const login = () => {
+    setTimeout(async () => {
+      let userData: any = await AsyncStorage.getItem('userData');
+      if (userData) {
+        userData = JSON.parse(userData);
+        if (
+          inputs.email == userData.email &&
+          inputs.password == userData.password
+        ) {
+          setInputs({
+            email: '',
+            password: '',
+          });
+
+          navigation.navigate('Home');
+          AsyncStorage.setItem(
+            'userData',
+            JSON.stringify({...userData, loggedIn: true}),
+          );
+        } else {
+          ToastAndroid.show(
+            'Email OR Password Wrong Try Again !',
+            ToastAndroid.SHORT,
+          );
+        }
+      } else {
+        ToastAndroid.show('User Does Not Exist!', ToastAndroid.SHORT);
+      }
+    }, 1000);
+  };
+
+  const handleOnchange = (text: any, input: any) => {
+    setInputs(prevState => ({...prevState, [input]: text}));
+  };
+
+  return (
+    <DismissKeyboard>
+      <View style={{flex: 1, backgroundColor: 'white'}}>
+        <Image
           style={{
-            height: 55,
-            backgroundColor: COLOURS.light,
-            flexDirection: 'row',
-            paddingHorizontal: 15,
-            borderWidth: 0.5,
-            width: '86%',
-            borderRadius: 5,
-            marginBottom: 20,
-          }}>
-          <TextInput
-            style={{
-              height: 55,
-              width: '86%',
-              color: COLOURS.black,
-              fontSize: 14,
-            }}
-            placeholderTextColor="#000"
-            placeholder="Password"
-            secureTextEntry={true}
-          />
-        </View>
-      </View>
-      <View
-        style={{
-          height: '8%',
-          width: '100%',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <TouchableOpacity
-          onPress={() => navigation.navigate('Home')}
-          style={{
-            width: '86%',
-            height: '90%',
-            backgroundColor: COLOURS.blue,
-            borderRadius: 5,
-            justifyContent: 'center',
-            alignItems: 'center',
+            width: 200,
+            height: 200,
+            alignSelf: 'center',
             marginTop: 50,
-            marginBottom: 10,
-          }}>
+            borderRadius: 50,
+          }}
+          source={require('../database/images/login/logo.png')}
+        />
+        <View style={{alignItems: 'center', marginTop: 70}}>
           <Text
             style={{
-              fontSize: 20,
-              fontWeight: '500',
-              letterSpacing: 1,
-              color: COLOURS.white,
+              color: COLOURS.blue,
+              fontSize: 40,
+              fontWeight: '800',
+              fontFamily: 'roboto',
+              letterSpacing: 1.5,
+              marginBottom: 20,
             }}>
             Sign In
           </Text>
-        </TouchableOpacity>
-        <View style={{flexDirection: 'row'}}>
-          <Text
+          <View
             style={{
-              color: COLOURS.black,
-              fontWeight: 'bold',
-              textAlign: 'center',
-              fontSize: 16,
+              height: 55,
+              backgroundColor: COLOURS.light,
+              flexDirection: 'row',
+              paddingHorizontal: 15,
+              borderWidth: 0.5,
+              width: '86%',
+              borderRadius: 5,
+              marginBottom: 20,
             }}>
-            Don't have an account ?{'  '}
-          </Text>
-          <Text
-            onPress={() => navigation.navigate('SignUp')}
+            <TextInput
+              onChangeText={text => handleOnchange(text, 'email')}
+              style={{
+                height: 55,
+                width: '86%',
+                color: COLOURS.black,
+                fontSize: 14,
+              }}
+              placeholderTextColor="#000"
+              placeholder="Email"
+            />
+          </View>
+
+          <View
             style={{
-              color: COLOURS.blue,
-              fontWeight: 'bold',
-              textAlign: 'center',
-              fontSize: 16,
+              height: 55,
+              backgroundColor: COLOURS.light,
+              flexDirection: 'row',
+              paddingHorizontal: 15,
+              borderWidth: 0.5,
+              width: '86%',
+              borderRadius: 5,
+              marginBottom: 20,
             }}>
-            Register Now
-          </Text>
+            <TextInput
+              onChangeText={text => handleOnchange(text, 'password')}
+              style={{
+                height: 55,
+                width: '86%',
+                color: COLOURS.black,
+                fontSize: 14,
+              }}
+              placeholderTextColor="#000"
+              placeholder="Password"
+              secureTextEntry={true}
+            />
+          </View>
+        </View>
+        <View
+          style={{
+            height: '8%',
+            width: '100%',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <TouchableOpacity
+            onPress={validate}
+            style={{
+              width: '86%',
+              height: '90%',
+              backgroundColor: COLOURS.blue,
+              borderRadius: 5,
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 50,
+              marginBottom: 10,
+            }}>
+            <Text
+              style={{
+                fontSize: 20,
+                fontWeight: '500',
+                letterSpacing: 1,
+                color: COLOURS.white,
+              }}>
+              Sign In
+            </Text>
+          </TouchableOpacity>
+          <View style={{flexDirection: 'row'}}>
+            <Text
+              style={{
+                color: COLOURS.black,
+                fontWeight: 'bold',
+                textAlign: 'center',
+                fontSize: 16,
+              }}>
+              Don't have an account ?{'  '}
+            </Text>
+            <Text
+              onPress={() => navigation.navigate('SignUp')}
+              style={{
+                color: COLOURS.blue,
+                fontWeight: 'bold',
+                textAlign: 'center',
+                fontSize: 16,
+              }}>
+              Register Now
+            </Text>
+          </View>
         </View>
       </View>
-    </View>
+    </DismissKeyboard>
   );
 };
 
--- src/components/screens/SignUp.tsx
+++ src/components/screens/SignUp.tsx
@@ -1,175 +1,271 @@
-import {View, Text, TouchableOpacity, Image, TextInput} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  TextInput,
+  ToastAndroid,
+} from 'react-native';
+import React, {useState} from 'react';
 import {COLOURS} from '../database/Database';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import DismissKeyboard from '../components/DismissKeyboard';
 
 const SignUp = ({navigation}: any) => {
-  return (
-    <View style={{flex: 1, backgroundColor: 'white'}}>
-      <Image
-        style={{
-          width: 200,
-          height: 200,
-          alignSelf: 'center',
-          marginTop: 50,
-          borderRadius: 50,
-        }}
-        source={require('../database/images/login/logo.png')}
-      />
-      <View style={{alignItems: 'center', marginTop: 70}}>
-        <Text
-          style={{
-            color: COLOURS.blue,
-            fontSize: 40,
-            fontWeight: '800',
-            fontFamily: 'roboto',
-            letterSpacing: 1.5,
-            marginBottom: 20,
-          }}>
-          Sign Up
-        </Text>
-        <View
-          style={{
-            height: 55,
-            backgroundColor: COLOURS.light,
-            flexDirection: 'row',
-            paddingHorizontal: 15,
-            borderWidth: 0.5,
-            width: '86%',
-            borderRadius: 5,
-            marginBottom: 10,
-          }}>
-          <TextInput
-            style={{
-              height: 55,
-              width: '86%',
-              color: COLOURS.black,
-              fontSize: 14,
-            }}
-            placeholderTextColor="#000"
-            placeholder="Name"
-          />
-        </View>
-        <View
-          style={{
-            height: 55,
-            backgroundColor: COLOURS.light,
-            flexDirection: 'row',
-            paddingHorizontal: 15,
-            borderWidth: 0.5,
-            width: '86%',
-            borderRadius: 5,
-            marginBottom: 10,
-          }}>
-          <TextInput
-            style={{
-              height: 55,
-              width: '86%',
-              color: COLOURS.black,
-              fontSize: 14,
-            }}
-            placeholderTextColor="#000"
-            placeholder="Phone Number"
-          />
-        </View>
-        <View
-          style={{
-            height: 55,
-            backgroundColor: COLOURS.light,
-            flexDirection: 'row',
-            paddingHorizontal: 15,
-            borderWidth: 0.5,
-            width: '86%',
-            borderRadius: 5,
-            marginBottom: 10,
-          }}>
-          <TextInput
-            style={{
-              height: 55,
-              width: '86%',
-              color: COLOURS.black,
-              fontSize: 14,
-            }}
-            placeholderTextColor="#000"
-            placeholder="Email"
-          />
-        </View>
+  interface Errors {
+    fullName: string;
+    phoneNo: string;
+    email: string;
+    password: string;
+  }
 
-        <View
+  const [inputs, setInputs] = useState({
+    fullName: '',
+    phoneNo: '',
+    email: '',
+    password: '',
+  });
+
+  const [errors, setErrors] = useState<Errors>({
+    fullName: '',
+    phoneNo: '',
+    email: '',
+    password: '',
+  });
+
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  const register = () => {
+    setTimeout(() => {
+      try {
+        AsyncStorage.setItem('userData', JSON.stringify(inputs));
+        setInputs({
+          fullName: '',
+          phoneNo: '',
+          email: '',
+          password: '',
+        });
+        navigation.navigate('SignIn');
+        ToastAndroid.show('User Register Completed.', ToastAndroid.SHORT);
+      } catch (error) {
+        ToastAndroid.show('Something went wrong', ToastAndroid.SHORT);
+      }
+    }, 3000);
+  };
+
+  const handleOnchange = (text: any, input: any) => {
+    setInputs(prevState => ({...prevState, [input]: text}));
+
+    let isValid = true;
+
+    if (!inputs.email) {
+      isValid = false;
+    }
+
+    if (!inputs.fullName) {
+      isValid = false;
+    }
+
+    if (!inputs.phoneNo) {
+      isValid = false;
+    }
+
+    if (!inputs.password) {
+      isValid = false;
+    }
+
+    setIsFormValid(isValid);
+  };
+
+  return (
+    <DismissKeyboard>
+      <View style={{flex: 1, backgroundColor: 'white'}}>
+        <Image
           style={{
-            height: 55,
-            backgroundColor: COLOURS.light,
-            flexDirection: 'row',
-            paddingHorizontal: 15,
-            borderWidth: 0.5,
-            width: '86%',
-            borderRadius: 5,
-            marginBottom: 20,
-          }}>
-          <TextInput
-            style={{
-              height: 55,
-              width: '86%',
-              color: COLOURS.black,
-              fontSize: 14,
-            }}
-            placeholderTextColor="#000"
-            placeholder="Password"
-            secureTextEntry={true}
-          />
-        </View>
-      </View>
-      <View
-        style={{
-          height: '8%',
-          width: '100%',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>
-        <TouchableOpacity
-          onPress={() => navigation.navigate('Home')}
-          style={{
-            width: '86%',
-            height: '90%',
-            backgroundColor: COLOURS.blue,
-            borderRadius: 5,
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginTop: 40,
-            marginBottom: 10,
-          }}>
+            width: 200,
+            height: 200,
+            alignSelf: 'center',
+            marginTop: 50,
+            borderRadius: 50,
+          }}
+          source={require('../database/images/login/logo.png')}
+        />
+        <View style={{alignItems: 'center', marginTop: 20}}>
           <Text
-            style={{
-              fontSize: 20,
-              fontWeight: '500',
-              letterSpacing: 1,
-              color: COLOURS.white,
-            }}>
-            SIGN Up
-          </Text>
-        </TouchableOpacity>
-        <View style={{flexDirection: 'row'}}>
-          <Text
-            style={{
-              color: COLOURS.black,
-              fontWeight: 'bold',
-              textAlign: 'center',
-              fontSize: 16,
-            }}>
-            Don't have an account ?{'  '}
-          </Text>
-          <Text
-            onPress={() => navigation.navigate('SignIn')}
             style={{
               color: COLOURS.blue,
-              fontWeight: 'bold',
-              textAlign: 'center',
-              fontSize: 16,
+              fontSize: 40,
+              fontWeight: '800',
+              letterSpacing: 1.5,
+              marginBottom: 20,
             }}>
-            Sign In
+            Sign Up
           </Text>
+          <View
+            style={{
+              height: 55,
+              backgroundColor: COLOURS.light,
+              flexDirection: 'row',
+              paddingHorizontal: 15,
+              borderWidth: 1,
+              width: '86%',
+              borderRadius: 5,
+              marginBottom: 10,
+              borderColor: !inputs.fullName.match(/^(?! *$)[a-zA-Z0-9 ]*$/)
+                ? 'red'
+                : 'black',
+            }}>
+            <TextInput
+              onChangeText={text => handleOnchange(text, 'fullName')}
+              style={{
+                height: 55,
+                width: '86%',
+                color: COLOURS.black,
+                fontSize: 14,
+              }}
+              placeholderTextColor="#000"
+              placeholder="Enter Your Full Name"
+            />
+            <Text style={{color: 'red'}}>{errors.fullName}</Text>
+          </View>
+          <View
+            style={{
+              height: 55,
+              backgroundColor: COLOURS.light,
+              flexDirection: 'row',
+              paddingHorizontal: 15,
+              borderWidth: 1,
+              width: '86%',
+              borderRadius: 5,
+              marginBottom: 10,
+              borderColor: !inputs.phoneNo.match(/^(?!0+$)(?:0\d{9}|94\d{9})$/)
+                ? 'red'
+                : 'black',
+            }}>
+            <TextInput
+              onChangeText={text => handleOnchange(text, 'phoneNo')}
+              style={{
+                height: 55,
+                width: '86%',
+                color: COLOURS.black,
+                fontSize: 14,
+              }}
+              keyboardType="numeric"
+              placeholderTextColor="#000"
+              placeholder="Enater Your Phone Number"
+            />
+          </View>
+          <View
+            style={{
+              height: 55,
+              backgroundColor: COLOURS.light,
+              flexDirection: 'row',
+              paddingHorizontal: 15,
+              borderWidth: 1,
+              width: '86%',
+              borderRadius: 5,
+              marginBottom: 10,
+              borderColor: !inputs.email.match(
+                /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+              )
+                ? 'red'
+                : 'black',
+            }}>
+            <TextInput
+              onChangeText={text => handleOnchange(text, 'email')}
+              style={{
+                height: 55,
+                width: '86%',
+                color: COLOURS.black,
+                fontSize: 14,
+              }}
+              placeholderTextColor="#000"
+              placeholder="Enter Your Email Address"
+            />
+          </View>
+
+          <View
+            style={{
+              height: 55,
+              backgroundColor: COLOURS.light,
+              flexDirection: 'row',
+              paddingHorizontal: 15,
+              borderWidth: 1,
+              width: '86%',
+              borderRadius: 5,
+              marginBottom: 20,
+              borderColor: !inputs.password.match(/^.{5,}$/) ? 'red' : 'black',
+            }}>
+            <TextInput
+              onChangeText={text => handleOnchange(text, 'password')}
+              style={{
+                height: 55,
+                width: '86%',
+                color: COLOURS.black,
+                fontSize: 14,
+              }}
+              placeholderTextColor="#000"
+              placeholder="Enter Your Password"
+              secureTextEntry={true}
+            />
+          </View>
+        </View>
+        <View
+          style={{
+            height: '8%',
+            width: '100%',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <TouchableOpacity
+            onPress={register}
+            disabled={!isFormValid}
+            style={{
+              width: '86%',
+              height: '90%',
+              backgroundColor: isFormValid
+                ? COLOURS.blue
+                : COLOURS.backgroundLight,
+              borderRadius: 5,
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginTop: 40,
+              marginBottom: 10,
+            }}>
+            <Text
+              style={{
+                fontSize: 20,
+                fontWeight: '500',
+                letterSpacing: 1,
+                color: COLOURS.white,
+              }}>
+              SIGN Up
+            </Text>
+          </TouchableOpacity>
+          <View style={{flexDirection: 'row'}}>
+            <Text
+              style={{
+                color: COLOURS.black,
+                fontWeight: 'bold',
+                textAlign: 'center',
+                fontSize: 16,
+              }}>
+              Don't have an account ?{'  '}
+            </Text>
+            <Text
+              onPress={() => navigation.navigate('SignIn')}
+              style={{
+                color: COLOURS.blue,
+                fontWeight: 'bold',
+                textAlign: 'center',
+                fontSize: 16,
+              }}>
+              Sign In
+            </Text>
+          </View>
         </View>
       </View>
-    </View>
+    </DismissKeyboard>
   );
 };
 
